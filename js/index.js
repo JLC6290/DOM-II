e46e69da7ac4adf9a580626fa16165e8997248c0
James <jlc6290@gmail.com>
2020-03-11 10:54:04 -0500

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-// Your code goes here
+// // Your code goes here
+
+// function clickEventHandler(event) {
+//     console.log(event.type + '!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+//     console.log(event.target)
+//   }
+//   document.querySelector('a').addEventListener('click', clickEventHandler)
+//   document.querySelector('nav').addEventListener('click', clickEventHandler)
+//   document.querySelector('header').addEventListener('click', clickEventHandler)
+//   document.body.addEventListener('click', clickEventHandler)
+//   document.addEventListener('click', clickEventHandler)
+//   window.addEventListener('click', clickEventHandler)
+
+
+navHover = document.querySelectorAll("a");
+function clickEventHandler(event) {
+    body.style.backgroundColor = 'red';
+}
+navHover[0].addEventListener('mouseover', clickEventHandler);
