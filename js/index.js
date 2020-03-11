c92f962904acadddf7c9b5c53152abcf958f6b1c
James <jlc6290@gmail.com>
2020-03-11 12:25:29 -0500

    Some event listeners working

--- js/index.js
+++ js/index.js
@@ -12,8 +12,34 @@
 //   window.addEventListener('click', clickEventHandler)
 
 
-navHover = document.querySelectorAll("a");
-function clickEventHandler(event) {
-    body.style.backgroundColor = 'red';
+
+let paragraphs = document.querySelectorAll("p");
+paragraphs.forEach(function (item){
+    item.style.visibility = "hidden";
+});
+let images = document.querySelectorAll("img");
+images.forEach(function (item){
+    item.style.visibility = "hidden";});
+
+document.querySelector("p").addEventListener('mouseover', showText);
+function showText(event) {
+    event.style.visibility = "visible";
 }
-navHover[0].addEventListener('mouseover', clickEventHandler);
+
+function redBackground(event) {
+    document.querySelector("body").style.backgroundColor = 'red';
+}
+document.querySelector("a").addEventListener('mouseover', redBackground);
+
+document.querySelector('.intro img').addEventListener('mousedown', (event) => {
+    event.target.style.visibility = 'visible';
+})
+
+document.querySelector('h1').addEventListener('mousedown', (event) => {
+    event.target.style.fontSize = '6rem'
+})
+
+function clearBackground(event) {
+    document.querySelector("body").style.backgroundColor = 'white';
+}
+document.querySelector("header").addEventListener('click', clearBackground);
